db432dd0068aa01a1ef1b9adf94371d3b181dce0
Nick Messer <messer185@gmail.com>
2022-06-30 18:10:16 -0400

    cleaned up some code, made it work between dims, and added a new texture.

--- src/main/java/net/messer/remote_controlled/item/custom/RemoteControl.java
+++ src/main/java/net/messer/remote_controlled/item/custom/RemoteControl.java
@@ -1,63 +1,48 @@
 package net.messer.remote_controlled.item.custom;
 
-import net.messer.remote_controlled.RemoteControlled;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.FurnaceBlockEntity;
-import net.minecraft.block.entity.LockableContainerBlockEntity;
-import net.minecraft.block.entity.LootableContainerBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.item.Items;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.screen.ForgingScreenHandler;
-import net.minecraft.screen.FurnaceScreenHandler;
 import net.minecraft.screen.NamedScreenHandlerFactory;
-import net.minecraft.screen.ScreenHandlerFactory;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 
+import java.util.Objects;
 
 
 public class RemoteControl extends Item {
-    BlockPos storedPositon;
+    BlockPos blockPosition;
+    String blockWorldID;
 
     public RemoteControl(Settings settings) {
         super(settings);
     }
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if(!world.isClient)
-            super.use(world, user, hand);
+        if(world.isClient)
+            return super.use(world, user, hand);
 
         var stackInHand = user.getStackInHand(hand);
         if(user.isSneaking())
         {
             BlockHitResult lookingAt = (BlockHitResult) user.raycast(10,0,true);
             BlockState foundBlockState = world.getBlockState(lookingAt.getBlockPos());
-            BlockEntity foundBlockEntity = world.getBlockEntity(lookingAt.getBlockPos());
 
-            if(foundBlockState.getBlock() == Blocks.AIR)
-                return super.use(world, user, hand);
+            if(foundBlockState == null || foundBlockState.getBlock() == Blocks.AIR)
+                return TypedActionResult.fail(stackInHand);
 
-            if(foundBlockEntity instanceof NamedScreenHandlerFactory screen){
-                write_npt(stackInHand, lookingAt);
-                return super.use(world, user, hand);
-            }
-            if(foundBlockState != null){
-                write_npt(stackInHand,lookingAt);
-                return super.use(world, user, hand);
-            }
+            write_npt(stackInHand,lookingAt, world);
 
             return super.use(world, user, hand);
         }
@@ -65,26 +50,33 @@ public class RemoteControl extends Item {
         if(stackInHand.hasNbt()){
             this.read_npt(stackInHand);
             BlockHitResult lookingAt = (BlockHitResult) user.raycast(10,0,true);
-            var blockState = world.getBlockState(storedPositon);
-            var blockEntity = world.getBlockEntity(storedPositon);
+            var blockWorld = Objects.requireNonNull(world.getServer()).getWorld(RegistryKey.of(Registry.WORLD_KEY, new Identifier(blockWorldID)));
 
-            if(blockState.getBlock() == Blocks.AIR)
+            if(blockWorld == null) {
                 clear_nbt(stackInHand);
+                user.sendMessage(new LiteralText("Block cannot be found."), true);
+                return TypedActionResult.fail(stackInHand);
+            }
+
+            var blockState = blockWorld.getBlockState(blockPosition);
+            var blockEntity = blockWorld.getBlockEntity(blockPosition);
+
+            if(blockState == null || blockState.getBlock() == Blocks.AIR)
+            {
+                clear_nbt(stackInHand);
+                user.sendMessage(new LiteralText("Block cannot be found."), true);
+                return TypedActionResult.fail(stackInHand);
+            }
 
             if(blockEntity instanceof NamedScreenHandlerFactory)
             {
-                user.openHandledScreen((NamedScreenHandlerFactory) world.getBlockEntity(storedPositon));
-            }
-            else if(blockState != null)
-            {
-                blockState.getBlock().onUse(blockState, world, storedPositon, user, hand, lookingAt);
+                user.openHandledScreen((NamedScreenHandlerFactory) blockWorld.getBlockEntity(blockPosition));
             }
             else{
-                clear_nbt(stackInHand);
-                user.sendMessage(new LiteralText("Block cannot be found."), true);
+                blockState.getBlock().onUse(blockState, blockWorld, blockPosition, user, hand, lookingAt);
             }
         }
-        return super.use(world, user, hand);
+        return TypedActionResult.success(stackInHand,true);
     }
 
     @Override
@@ -93,21 +85,24 @@ public class RemoteControl extends Item {
     }
 
     public void clear_nbt(ItemStack stack){
-        var nbt = stack.getNbt();
+        var nbt = stack.getOrCreateNbt();
         nbt.remove("pos");
+        nbt.remove("world");
         stack.setNbt(nbt);
     }
 
-    public void write_npt(ItemStack stack, BlockHitResult hitResult){
+    public void write_npt(ItemStack stack, BlockHitResult hitResult, World worldOfBlock){
         var nbt = stack.getOrCreateNbt();
         if(hitResult!= null){
             nbt.putLong("pos", hitResult.getBlockPos().asLong());
+            nbt.putString("world", worldOfBlock.getRegistryKey().getValue().toString());
         }
         stack.setNbt(nbt);
     }
 
     public void read_npt(ItemStack stack){
         var nbt = stack.getOrCreateNbt();
-        this.storedPositon = BlockPos.fromLong(nbt.getLong("pos"));
+        this.blockPosition = BlockPos.fromLong(nbt.getLong("pos"));
+        this.blockWorldID = nbt.getString("world");
     }
 }
